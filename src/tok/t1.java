1511535b86f4fbd642e2a9df7b9f6d0ea5d65046
hp <hp@DESKTOP-VV5CTN2>
2025-07-23 23:25:56 +0530

    first change commit

--- src/tok/t1.java
+++ src/tok/t1.java
@@ -5,6 +5,13 @@ public class t1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		String hungry ="yes";
+		if (hungry=="yes") {
+			 System.out.print("eat ");
+		}
+		else {
+			System.out.print("work ");
+		}
 	}
 
 }
